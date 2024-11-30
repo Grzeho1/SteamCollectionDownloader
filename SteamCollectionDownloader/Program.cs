8364e335f38c1366a1a1f3eaf7f2085f4d5b8006
rehyy5@seznam.cz <rehyy5@seznam.cz>
2024-11-30 20:53:12 +0100

    Přidejte soubory projektu.

---
+++ SteamCollectionDownloader/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using SteamDownloader;
+
+
+class Program
+{
+
+
+    static async Task Main(string[] args)
+    {
+        string steamCmdPath = "D:\\steamcmd\\steamcmd.exe";
+        Console.WriteLine(steamCmdPath);
+
+        Console.WriteLine("Insert collection url.");
+        string collectionUrl = Console.ReadLine();
+
+        List<string> list = await Scraper.ExtractWorkshopIDs(collectionUrl);
+
+        Console.WriteLine("IDs:");
+        List<string> commands = new List<string>();
+
+        // všechny stahované položky
+        foreach (string id in list)
+        {
+            Console.WriteLine("workshop_download_item" + " " + id);
+            commands.Add($"+workshop_download_item {id}");
+        }
+
+
+        string allCommands = string.Join(" ", commands);
+
+        ProcessStartInfo startInfo = new ProcessStartInfo()
+        {
+            FileName = steamCmdPath,
+            Arguments = $"+login anonymous {allCommands} +quit",  // prihlaseni jako anonym
+
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = false
+        };
+
+        using (Process process = Process.Start(startInfo))
+        {
+            if (process != null)
+            {
+                try
+                {
+                    string output = await process.StandardOutput.ReadLineAsync();
+                    Console.WriteLine(output);
+                    process.WaitForExit();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+                finally
+                {
+                    if (!process.HasExited)
+                    {
+                        process.Kill();
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SteamCollectionDownloader/Scraper.cs
@@ -0,0 +1,61 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamDownloader
+{
+    public class Scraper
+    {
+
+        public static async Task<List<string>> ExtractWorkshopIDs(string collectionUrl)
+        {
+            var ids = new List<string>();
+
+            using (HttpClient client = new HttpClient())
+            {
+
+                Console.WriteLine("Downloading ids from collection");
+                var html = await client.GetStringAsync(collectionUrl);
+
+                var doc = new HtmlDocument();
+                doc.LoadHtml(html);
+
+                // hledaní id v html
+                var nodes = doc.DocumentNode.SelectNodes("//div[@class='collectionItem']/@id");
+
+                // hledaní gameId v html
+                var gameIdNode = doc.DocumentNode.SelectSingleNode("//a[contains(@href, 'app/')]");
+
+                string gameId = gameIdNode != null ? gameIdNode.GetAttributeValue("href", "").Split('/').Last().Split('?')[0] : "";
+
+                if (nodes != null && !string.IsNullOrEmpty(gameId))
+                {
+                    foreach (var node in nodes)
+                    {
+                        string idValue = node.GetAttributeValue("id", "");
+
+                        if (idValue.StartsWith("sharedfile_"))
+                        {
+                            // osekat prefix a extrahovat jen id
+                            string formattedId = $"{gameId} {idValue.Replace("sharedfile_", "")}";
+                            ids.Add(formattedId);
+
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No id in collection.");
+                }
+
+
+            }
+
+            return ids;
+        }
+    }
+}
